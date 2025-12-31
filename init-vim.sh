fbeea1dd34d3f20b627b7f9b31ab543eeb7388b6
uyiromo <yudevadm@gmail.com>
2025-12-31 20:56:58 +0900

    disable vim copilot

--- init-vim.sh
+++ init-vim.sh
@@ -17,8 +17,8 @@ if [[ ! -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
 fi
 
-if [[ ! -d "${HOME}/.vim/pack/github/start/copilot.vim" ]]; then
-    git clone https://github.com/github/copilot.vim.git "${HOME}/.vim/pack/github/start/copilot.vim"
-fi
+#if [[ ! -d "${HOME}/.vim/pack/github/start/copilot.vim" ]]; then
+#    git clone https://github.com/github/copilot.vim.git "${HOME}/.vim/pack/github/start/copilot.vim"
+#fi
 
 vim +PluginInstall +qall
